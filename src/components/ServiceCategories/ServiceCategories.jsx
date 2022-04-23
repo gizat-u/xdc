57c3b4fe7cca8f10023ef8936386d7a8e6430e7b
gizat-u <giza_uteev@mail.ru>
2022-04-23 21:42:37 +0300

    v2.0

--- src/components/ServiceCategories/ServiceCategories.jsx
+++ src/components/ServiceCategories/ServiceCategories.jsx
@@ -1,15 +1,34 @@
 import "./ServiceCategories.css";
 
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 
 import { motion } from "framer-motion";
 import { Link } from 'react-router-dom';
 
 import { useTranslation } from "react-i18next";
 
+import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
 
 export const ServiceCategories = ({ services, path }) => {
+
+	const [menu, setMenu] = useState(false);
+	const [width, setWidth] = useState(window.innerWidth);
+
+	useEffect(() => {
+		function handleResize() {
+			setWidth(window.innerWidth);
+		}
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, [width]);
+
+	const menuToggleHandler = () => {
+		setMenu((p) => !p);
+	}
+
+	const handleClickLink = () => {
+		setMenu(false);
+	}
 
 	const pathname = "" + path + "";
 	const { t } = useTranslation();
@@ -30,11 +49,18 @@ export const ServiceCategories = ({ services, path }) => {
 
 	return (
 		<div className="service_categories" >
-			<div className="service_categories_content">
+			<button className="services_list_btn" onClick={() => menuToggleHandler()}>
+				{menu ?
+					<AiOutlineCaretUp color="red" />
+					:
+					<AiOutlineCaretDown />
+				}
+			</button>
+			<div className={`service_categories_content ${menu ? 'open' : ''} `}>
 				{services.map(({ id, link, text }) => {
 					return (
 						<motion.span key={id} variants={animateServices} initial="hidden" animate="visible" custom={id} >
-							<Link to={link} className={link === pathname ? "active" : ""}>
+							<Link to={link} onClick={() => handleClickLink()} className={link === pathname ? "active" : ""}>
 								{t(text)}
 							</Link>
 						</motion.span>
