2d111f5e2595a7e59f5ae7ec8a8e95726028ed11
gizat-u <giza_uteev@mail.ru>
2022-05-03 17:48:23 +0300

    v2.5

--- src/components/ServiceImages/ServiceImages.jsx
+++ src/components/ServiceImages/ServiceImages.jsx
@@ -6,9 +6,9 @@ import { motion } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-cube";
-import "swiper/css/scrollbar";
+import "swiper/css/pagination";
 
-import { Autoplay, EffectCube, Scrollbar } from "swiper";
+import { Autoplay, EffectCube, Pagination } from "swiper";
 
 export const ServiceImages = ({ service }) => {
 
@@ -32,9 +32,7 @@ export const ServiceImages = ({ service }) => {
 			<Swiper
 				effect={"cube"}
 				grabCursor={true}
-				scrollbar={{
-					hide: true,
-				}}
+				pagination={true}
 				autoplay={{
 					delay: 2000,
 					disableOnInteraction: false,
@@ -45,7 +43,7 @@ export const ServiceImages = ({ service }) => {
 					shadowOffset: 20,
 					shadowScale: 0.94,
 				}}
-				modules={[Autoplay, EffectCube, Scrollbar]}
+				modules={[Autoplay, EffectCube, Pagination]}
 				className="mySwiper"
 			>
 				<SwiperSlide><img src={service.img_1} alt="service" /></SwiperSlide>
