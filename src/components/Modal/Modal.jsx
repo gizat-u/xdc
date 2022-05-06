753b34493f1c7c198480a2421d25d66c357ae735
gizat-u <giza_uteev@mail.ru>
2022-05-06 21:55:30 +0300

    v4.0

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,10 @@
 import "./Modal.css";
 
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import emailjs from '@emailjs/browser';
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
-import logo from "../../assets/images/logo.png";
+import swal from 'sweetalert';
 
 
 export default function Modal({ type_service }) {
@@ -11,10 +12,36 @@ export default function Modal({ type_service }) {
 	const { t } = useTranslation();
 
 	const [modal, setModal] = useState(false);
+	const [value, setValue] = useState()
 
 	const modalToggleHandler = () => {
 		setModal((p) => !p);
 	}
+
+
+	const closeModalToggleHanlder = () => {
+		setModal((p) => !p);
+	}
+
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs.sendForm('service_099vb0n', 'template_fowcwqe', form.current, 'uPB51jN-5Gnk8oGGj')
+			.then((result) => {
+				console.log(result.text);
+				swal("You have signed up", "", "success");
+				setModal((p) => !p);
+				e.target.reset();
+			}, (error) => {
+				console.log(error.text);
+				swal("Error", error.text, "error");
+				setModal((p) => !p);
+				e.target.reset();
+			});
+	};
+
 
 	const animateDiv = {
 		hidden: {
@@ -32,18 +59,33 @@ export default function Modal({ type_service }) {
 
 
 	return (
-		<motion.div className="content_el_in" variants={animateDiv} initial="hidden" animate="visible" >
-			<div className={`modal ${modal ? 'open' : ''} `}>
-
-				<label>Name</label>
-				<input type="text" name="user_name" />
-				<label>Email</label>
-				<input type="email" name="user_email" />
-				<label>Message</label>
-				<textarea name="message" />
-				<input type="submit" value="Send" />
+		<motion.div className="content_el_in modal_in" variants={animateDiv} initial="hidden" animate="visible" >
+			<div className={`modal ${modal ? 'open' : ''} `} onClick={closeModalToggleHanlder}>
+				<div onClick={closeModalToggleHanlder}>
+					<form className="modal_inner" ref={form} onSubmit={sendEmail}>
+						<div className="modal_item modal_title">
+							<h3>Submit your application</h3>
+						</div>
+						<div className="modal_item">
+							<input type="text" placeholder="Name" name="user_name" />
+						</div>
+						<div className="modal_item">
+							<input type="tel" placeholder="Phone number" name="user_phone" />
+						</div>
+						<div className="modal_item">
+							<input type="text" placeholder="Vehicle model and number" name="message" />
+						</div>
+						<div className="modal_item modal_checkbox">
+							<input type="checkbox" placeholder="Name" name="subscribe_news" />
+							<label>Sign me up for newsletter</label>
+						</div>
+						<div className="modal_item">
+							<button type="submit">sign up</button>
+						</div>
+					</form>
+				</div>
 			</div>
-			<button onClick={() => modalToggleHandler()}>
+			<button onClick={modalToggleHandler}>
 				{t("Sign Up")}
 			</button>
 		</motion.div>
